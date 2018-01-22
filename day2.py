51a6b25d6ca4c8662a2a983d07c1ec3748cc16f2
Serene Liu <cloud9@sentanother.email>
2018-01-22 05:42:32 +0000

    reduced to just one bathroom_code function

--- day2.py
+++ day2.py
@@ -30,36 +30,21 @@ directions = {
     'L': left
 }
 
-def bathroom_code(some_input):
-    (x, y) = (1, 1)
+def bathroom_code(some_input, some_keypad, start):
+    (x, y) = start
     code = []
     for line in some_input:
         for i in xrange(len(line)):
             direction = line[i]
-            if (direction == 'U' and y > 0) or (direction == 'D' and y < 2) or (direction == 'R' and x < 2) or (direction == 'L' and x > 0):
-                (x, y) = directions[direction](x, y)
-            else:
-                (x, y) = (x, y)
-            if i == len(line) - 1:
-                code.append(keypad[y][x])
-    return ''.join(code)
-
-# print bathroom_code(example_input) # expect: 1985
-# print bathroom_code(puzzle_input)
-
-def bathroom_code_2(some_input):
-    (x, y) = (0, 2)
-    code = []
-    for line in some_input:
-        for i in xrange(len(line)):
-            direction = line[i]
-            if (direction == 'U' and y > 0) or (direction == 'D' and y < 4) or (direction == 'R' and x < 4) or (direction == 'L' and x > 0):
+            if (direction == 'U' and y > 0) or (direction == 'D' and y < (len(some_keypad) - 1)) or (direction == 'R' and x < (len(some_keypad) - 1)) or (direction == 'L' and x > 0):
                 (a, b) = directions[direction](x, y)
-                if actual_keypad[b][a] != ' ':
+                if some_keypad[b][a] != ' ':
                     (x, y) = (a, b)
             if i == len(line) - 1:
-                code.append(actual_keypad[y][x])
+                code.append(some_keypad[y][x])
     return ''.join(code)
 
-# print bathroom_code_2(example_input) # expect: 5DB3
-print bathroom_code_2(puzzle_input)
+# print bathroom_code(example_input, keypad, (1, 1)) # expect: 1985
+# print bathroom_code(puzzle_input, keypad, (1, 1))
+# print bathroom_code(example_input, actual_keypad, (0, 2)) # expect: 5DB3
+print bathroom_code(puzzle_input, actual_keypad, (0, 2))

357eb462ea3375a58eecbd37664525c2ad0199d1
Serene Liu <cloud9@sentanother.email>
2018-01-22 05:09:22 +0000

    Day 2: Bathroom Security | Both Parts Completed

--- day2.py
+++ day2.py
@@ -9,6 +9,8 @@ example_input = [
 
 keypad = ['123', '456', '789']
 
+actual_keypad = ['  1  ',  ' 234 ', '56789', ' ABC ', '  D  ']
+
 def up(x, y):
     return x, y - 1
 
@@ -43,4 +45,25 @@ def bathroom_code(some_input):
     return ''.join(code)
 
 # print bathroom_code(example_input) # expect: 1985
-print bathroom_code(puzzle_input)
+# print bathroom_code(puzzle_input)
+
+def bathroom_code_2(some_input):
+    (x, y) = (0, 2)
+    code = []
+    for line in some_input:
+        for i in xrange(len(line)):
+            direction = line[i]
+            if (direction == 'U' and y > 0) or (direction == 'D' and y < 4) or (direction == 'R' and x < 4) or (direction == 'L' and x > 0):
+                (a, b) = directions[direction](x, y)
+                if actual_keypad[b][a] != ' ':
+                    (x, y) = (a, b)
+                else:
+                    (x, y) = (x, y)
+            else:
+                (x, y) = (x, y)
+            if i == len(line) - 1:
+                code.append(actual_keypad[y][x])
+    return ''.join(code)
+
+# print bathroom_code_2(example_input) # expect: 5DB3
+print bathroom_code_2(puzzle_input)

f1b47eba127c0c7f69aac1f1228120f360472d36
Serene Liu <cloud9@sentanother.email>
2018-01-24 05:36:06 +0000

    Day 3: Squares With Three Sides | Both Parts Complete

--- day3.py
+++ day3.py
@@ -11,3 +11,16 @@ def find_valid_triangles(some_input):
     return valid_triangles
 
 print find_valid_triangles(puzzle_input)
+
+def find_vertical_triangles(some_input):
+    valid_triangles = 0
+    for j in xrange(3):
+        for i in xrange(0, len(some_input), 3):
+            a = some_input[i][j]
+            b = some_input[i+1][j]
+            c = some_input[i+2][j]
+            if a + b > c and b + c > a and a + c > b:
+                valid_triangles += 1
+    return valid_triangles
+
+print find_vertical_triangles(puzzle_input)

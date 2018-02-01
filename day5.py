2af345f804e0d0dca7f530e8604ab80d4b0d1c28
Serene Liu <cloud9@sentanother.email>
2018-02-01 19:36:47 +0000

    Day 5: How About a Nice Game of Chess? | Both Parts Completed

--- day5.py
+++ day5.py
@@ -3,14 +3,19 @@ import hashlib
 puzzle_input = 'abbhdwsy'
 
 def find_next_char(string):
+    pw_pos = {}
     password = []
     index = 0
-    while len(password) < 8:
+    while len(pw_pos) < 8:
         string_index = string + str(index)
         md5_hash = hashlib.md5(string_index.encode('utf-8')).hexdigest()
-        if md5_hash.startswith('00000'):
-            password.append(md5_hash[5])
+        if md5_hash.startswith('00000') and md5_hash[5] in '01234567' and md5_hash[5] not in pw_pos.keys():
+            pw_pos[md5_hash[5]] = md5_hash[6]
         index += 1
+    for i in xrange(8):
+        for k, v in pw_pos.items():
+            if k == str(i):
+                password.append(v)
     return ''.join(password)
 
 print find_next_char(puzzle_input)

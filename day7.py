c0eabd16b1dd8f63299be1431b6b5662a7aa3ea2
Serene Liu <cloud9@sentanother.email>
2018-02-02 18:37:07 +0000

    Day 7: Internet Protocol Version  7 | Both Parts Completed

--- day7.py
+++ day7.py
@@ -8,38 +8,71 @@ example_2 = 'abcd[bddb]xyyx' # does not support TLS (bddb is within square brack
 example_3 = 'aaaa[qwer]tyui' # does not support TLS (aaaa is invalid; the interior characters must be different)
 example_4 = 'ioxxoj[asdfgh]zxcvbn' # supports TLS (oxxo is outside square brackets, even though it's within a larger string)
 
-def contains_ABBA(seq):
+def split_ip(ip):
+    split_ip = re.split('\[|\]', ip)
+    hypernet_seq = []
+    supernet_seq = []
+    for seq in split_ip:
+        if '[' + seq + ']' in ip:
+            hypernet_seq.append(seq)
+        else:
+            supernet_seq.append(seq)
+    return hypernet_seq, supernet_seq
+
+def contains_abba(seq):
     for i in xrange(0, len(seq) - 3):
         if seq[i:i+2] == seq[i+3:i+1:-1] and seq[i] != seq[i+1]:
             return True
-
-def supports_TLS(IP):
-    split_IP = re.split('\[|\]', IP)
-    hypernet_seq = []
-    outside_seq = []
-    for seq in split_IP:
-        if '[' + seq + ']' in IP:
-            hypernet_seq.append(seq)
-        else:
-            outside_seq.append(seq)
-    for h in hypernet_seq:
-        if contains_ABBA(h):
-            return False
-    for o in outside_seq:
-        if contains_ABBA(o):
-            return True
     return False
 
-def count_IPs(IP_list):
-    IPs = 0
-    for IP in IP_list:
-        if supports_TLS(IP):
-            IPs += 1
-    return IPs
+def supports_tls(ip):
+    hypernet, supernet = split_ip(ip)
+    if any(contains_abba(h) for h in hypernet):
+        return False
+    if any(contains_abba(s) for s in supernet):
+        return True
+    return False
 
-# print supports_TLS(example_1)
-# print supports_TLS(example_2)
-# print supports_TLS(example_3)
-# print supports_TLS(example_4)
+# print supports_tls(example_1)
+# print supports_tls(example_2)
+# print supports_tls(example_3)
+# print supports_tls(example_4)
 
-print count_IPs(puzzle_input)
+def contains_aba(seq):
+    aba = []
+    for i in xrange(0, len(seq) - 2):
+        if seq[i] == seq[i+2] and seq[i] != seq[i+1]:
+            aba.append(seq[i:i+3])
+    return aba
+
+example_5 = 'aba[bab]xyz' # supports SSL (aba outside square brackets with corresponding bab within square brackets)
+example_6 = 'xyx[xyx]xyx' # does not support SSL (xyx, but no corresponding yxy)
+example_7 = 'aaa[kek]eke' # supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different)
+example_8 = 'zazbz[bzb]cdb' # supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap)
+
+def supports_ssl(ip):
+    hypernet, supernet = split_ip(ip)
+    for s in supernet:
+        for h in hypernet:
+            for aba in contains_aba(s):
+                for bab in contains_aba(h):
+                    if aba[0:2] == bab[0:2][::-1]:
+                        return True
+    return False
+
+# print supports_ssl(example_5)
+# print supports_ssl(example_6)
+# print supports_ssl(example_7)
+# print supports_ssl(example_8)
+
+def count_ips(ip_list):
+    tls_ips = 0
+    ssl_ips = 0
+    for ip in ip_list:
+        if supports_tls(ip):
+            tls_ips += 1
+        if supports_ssl(ip):
+            ssl_ips += 1
+    return tls_ips, ssl_ips
+
+print count_ips(puzzle_input)

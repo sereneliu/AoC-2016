0febb7f3e7ae6b8e466e89f651f94d87656551a6
Serene Liu <cloud9@sentanother.email>
2018-02-06 16:25:11 +0000

    Day 9: Explosives in Cyberspace | Part 2 Completed with a recursive function

--- day9.py
+++ day9.py
@@ -2,29 +2,33 @@ with open('day9.txt') as puzzle_file:
     puzzle_input = puzzle_file.read()
 
 def decompressed(file):
-    decompressed = False
+    is_decompressed = False
     decompressed_length = 0
     marker = []
+    to_be_decomp = []
     skip = 0
     for i in xrange(len(file)):
-        if not decompressed:
+        if not is_decompressed:
             if file[i] != '(':
                 decompressed_length += 1
             else:
-                decompressed = True
-        elif decompressed:
+                is_decompressed = True
+        elif is_decompressed:
             if file[i] != ')' and type(marker) != int:
                 marker.append(file[i])
             elif skip == 0:
                 marker = ''.join(marker)
                 skip = int(marker[:marker.index('x')]) + 1
-                marker = (int(marker[:marker.index('x')]) * int(marker[marker.index('x') + 1:]))
+                marker = int(marker[marker.index('x') + 1:])
             elif skip > 1:
+                to_be_decomp.append(file[i])
                 skip -= 1
             if skip == 1:
-                decompressed_length += marker
+                to_be_decomp = ''.join(to_be_decomp)
+                decompressed_length += decompressed(to_be_decomp) * marker
+                to_be_decomp = []
                 marker = []
-                decompressed = False
+                is_decompressed = False
                 skip -= 1
     return decompressed_length
 
